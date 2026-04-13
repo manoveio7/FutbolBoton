fa04146f8c242932ccaefc5174024de314baa0af
manoveio7 <85594759+manoveio7@users.noreply.github.com>
2026-04-12 22:50:00 -0300

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheName = 'futbol-boton-v1';
+const assets = [
+  './',
+  './index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
